98e3fb6401ff14644ce07954b5bd5407a19566e4
Luis Cruz <luisginocruz_95@hotmail.com>
2024-10-28 18:34:12 -0500

    Reapply "Push de trigger de los segmentos CB y FA"
    This reverts commit b19158582145839e04ae15dcf0c7bde0956bc82a.

---
+++ TRIGGER/FA/TR_fa_comision_realizadas.sql
@@ -0,0 +1,56 @@
+CREATE TRIGGER tr_fa_comision_realizadas  
+ON fa_comision_realizadas 
+AFTER INSERT,DELETE   
+AS 
+
+declare @codigo varchar(30)='',
+		@tipo char(1)
+
+IF EXISTS (SELECT * FROM inserted)
+BEGIN
+	
+	SELECT @codigo= pk_Id FROM inserted
+	order by pk_Id asc
+
+	SET @tipo ='I'
+
+END
+
+IF  EXISTS (SELECT * FROM deleted)
+BEGIN
+	SELECT @codigo= pk_Id FROM deleted
+	order by pk_Id asc
+
+	SET @tipo ='D'
+END
+
+IF @tipo IS NOT NULL AND @codigo !=''
+BEGIN
+	INSERT INTO temp_registroMigracion (nombre_table,tipo,codigo,[status])
+					VALUES('fa_comision_realizadas',@tipo,@codigo,1)
+END
+GO
+
+CREATE TRIGGER TR_fa_comision_realizadas_UP
+ON fa_comision_realizadas
+AFTER UPDATE
+AS
+
+	declare @codigo varchar(30)='',
+			@tipo char(1)
+
+BEGIN
+	SELECT @codigo= pk_Id FROM inserted
+	order by pk_Id asc
+
+	SET @tipo ='U'
+
+	IF @codigo !=''
+	BEGIN
+		INSERT INTO temp_registroMigracion (nombre_table,tipo,codigo,[status])
+					VALUES('fa_comision_realizadas',@tipo,@codigo,1)
+	END
+END
+
+	
+GO
