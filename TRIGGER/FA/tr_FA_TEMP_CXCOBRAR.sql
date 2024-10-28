08c36c688d3d32eba2e2a27d41d76a6e404f1281
Luis Cruz <luisginocruz_95@hotmail.com>
2024-10-28 18:35:30 -0500

    Reapply "Reapply "Push de trigger de los segmentos CB y FA""
    This reverts commit 4bf402c8c665e9aee08100e9f97058fad79da501.

---
+++ TRIGGER/FA/tr_FA_TEMP_CXCOBRAR.sql
@@ -0,0 +1,56 @@
+CREATE TRIGGER tr_FA_TEMP_CXCOBRAR  
+ON FA_TEMP_CXCOBRAR 
+AFTER INSERT,DELETE   
+AS 
+
+declare @codigo varchar(30)='',
+		@tipo char(1)
+
+IF EXISTS (SELECT * FROM inserted)
+BEGIN
+	
+	SELECT @codigo= pk_Id FROM inserted
+	order by pk_Id asc
+
+	SET @tipo ='I'
+
+END
+
+IF  EXISTS (SELECT * FROM deleted)
+BEGIN
+	SELECT @codigo= pk_Id FROM deleted
+	order by pk_Id asc
+
+	SET @tipo ='D'
+END
+
+IF @tipo IS NOT NULL AND @codigo !=''
+BEGIN
+	INSERT INTO temp_registroMigracion (nombre_table,tipo,codigo,[status])
+					VALUES('FA_TEMP_CXCOBRAR',@tipo,@codigo,1)
+END
+GO
+
+CREATE TRIGGER TR_FA_TEMP_CXCOBRAR_UP
+ON FA_TEMP_CXCOBRAR
+AFTER UPDATE
+AS
+
+	declare @codigo varchar(30)='',
+			@tipo char(1)
+
+BEGIN
+	SELECT @codigo= pk_Id FROM inserted
+	order by pk_Id asc
+
+	SET @tipo ='U'
+
+	IF @codigo !=''
+	BEGIN
+		INSERT INTO temp_registroMigracion (nombre_table,tipo,codigo,[status])
+					VALUES('FA_TEMP_CXCOBRAR',@tipo,@codigo,1)
+	END
+END
+
+	
+GO
---
+++ TRIGGER/FA/tr_FA_TIPO_TRANSACCION_UAF.sql
@@ -0,0 +1,56 @@
+CREATE TRIGGER tr_FA_TIPO_TRANSACCION_UAF  
+ON FA_TIPO_TRANSACCION_UAF 
+AFTER INSERT,DELETE   
+AS 
+
+declare @codigo varchar(30)='',
+		@tipo char(1)
+
+IF EXISTS (SELECT * FROM inserted)
+BEGIN
+	
+	SELECT @codigo= pk_Id FROM inserted
+	order by pk_Id asc
+
+	SET @tipo ='I'
+
+END
+
+IF  EXISTS (SELECT * FROM deleted)
+BEGIN
+	SELECT @codigo= pk_Id FROM deleted
+	order by pk_Id asc
+
+	SET @tipo ='D'
+END
+
+IF @tipo IS NOT NULL AND @codigo !=''
+BEGIN
+	INSERT INTO temp_registroMigracion (nombre_table,tipo,codigo,[status])
+					VALUES('FA_TIPO_TRANSACCION_UAF',@tipo,@codigo,1)
+END
+GO
+
+CREATE TRIGGER TR_FA_TIPO_TRANSACCION_UAF_UP
+ON FA_TIPO_TRANSACCION_UAF
+AFTER UPDATE
+AS
+
+	declare @codigo varchar(30)='',
+			@tipo char(1)
+
+BEGIN
+	SELECT @codigo= pk_Id FROM inserted
+	order by pk_Id asc
+
+	SET @tipo ='U'
+
+	IF @codigo !=''
+	BEGIN
+		INSERT INTO temp_registroMigracion (nombre_table,tipo,codigo,[status])
+					VALUES('FA_TIPO_TRANSACCION_UAF',@tipo,@codigo,1)
+	END
+END
+
+	
+GO
